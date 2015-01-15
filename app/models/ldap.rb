51581b0a39d581ed101294baf2683c6c083b6139
Wolfgang Hennerbichler <wogri@wogri.com>
2015-01-15 10:06:30 +0100

    lock version bugfix

--- app/models/ldap.rb
+++ app/models/ldap.rb
@@ -37,7 +37,11 @@ class Ldap < ActiveRecord::Base
 				:attributes => ldap_config['attribute_mapping'].values
 			) do |entry|
 				logger.debug(entry.inspect)
-				user.name = entry[ldap_to_userattrib["name"].downcase.to_sym].first.to_s
+        begin
+				  user.name = entry[ldap_to_userattrib["name"].downcase.to_sym].first.to_s
+        rescue 
+          logger.error("shit. user had a wrong encoding")
+        end
 				user.mailaddress = entry[ldap_to_userattrib["mailaddress"].downcase.to_sym].first.to_s
 				user.id = username
 				if static_group = ldap_config['static_group'] 
