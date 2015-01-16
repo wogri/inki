abff570dedab2451229ca61fe39c047a195652ce
Wolfgang Hennerbichler <wogri@wogri.com>
2015-01-16 08:36:40 +0100

    some fancy javascript

--- app/assets/javascripts/spinner_link.js
+++ app/assets/javascripts/spinner_link.js
@@ -3,16 +3,20 @@ $(document).ready(function() {
   // bind all elements that are links in the DOM to a click event 
   $('.spinner').click(function() { 
     $('#waiting_bar').fadeIn(); 
+    $('.navbar-brand').addClass('navbar-spinner')
   });
   // on every ajax-request that has completed, fade the waiting-bar element
   $(document).ajaxComplete(function() { 
     $('#waiting_bar').fadeOut(1000); 
+    $('.navbar-brand').removeClass('navbar-spinner')
     // but also re-bind the spinner-link events, otherwhise the freshly loaded ajax elements don't have this information
     $('.spinner').bind('click', function() { 
       $('#waiting_bar').fadeIn(); 
+      $('.navbar-brand').addClass('navbar-spinner')
     });
     $('.ui-dialog-titlebar-close').bind('click', function() {
       $('#waiting_bar').fadeOut(1000); 
+      $('.navbar-brand').removeClass('navbar-spinner')
     });
   });
 });
