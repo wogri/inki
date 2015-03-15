03049e2b7e08dd3788116553a755eb13d60e9e67
Wolfgang Hennerbichler <wogri@wogri.com>
2015-03-15 22:00:22 +0100

    added comment

--- app/models/latex_template.rb
+++ app/models/latex_template.rb
@@ -1,4 +1,5 @@
 class LatexTemplate < ActiveRecord::Base
+  # serves as a template storage for latex templates that contain ERB. Can be converted to PDF.
   has_icon "file-text-o"
   validates :model, uniqueness: true
 	is_versioned
