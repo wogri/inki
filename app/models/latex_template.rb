68d91d7b835955c9a01718877c80ebfe2af93c95
Wolfgang Hennerbichler <wogri@wogri.com>
2015-03-16 20:37:53 +0100

    template got truncation

--- app/models/latex_template.rb
+++ app/models/latex_template.rb
@@ -2,5 +2,6 @@ class LatexTemplate < ActiveRecord::Base
   # serves as a template storage for latex templates that contain ERB. Can be converted to PDF.
   has_icon "file-text-o"
   validates :model, uniqueness: true
+	attribute_properties :template => :truncate
 	is_versioned
 end
